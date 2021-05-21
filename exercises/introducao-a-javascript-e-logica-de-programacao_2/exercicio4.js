ddb06a107ff64b1d05beef40eadec9465f430632
Barros99 <jeanmeneses@hotmail.com>
2021-05-20 21:24:57 -0300

    exercicio 6 valores impares e exercicio 7 menor valor

--- exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio4.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio4.js
@@ -1,9 +1,11 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
-for (let index = 0; index < numbers.length; index += 1) {
-  soma = soma + numbers[index];
+let mediaAritmetica = 0;
+
+for (const iterator of numbers) {
+  soma += iterator;
 }
-let mediaAritmetica = soma / numbers.length;
+mediaAritmetica = soma / numbers.length;
 console.log("Soma = " + soma + "\nMédia = " + mediaAritmetica);
 if (mediaAritmetica > 20) {
   console.log("valor maior que 20");
