0eec3f399de9ad4bc32b26a41d12870b7b3e41d7
Barros99 <jeanmeneses@hotmail.com>
2021-05-22 22:32:03 -0300

    Correçao exercicio 7

--- exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio7.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio7.js
@@ -1,11 +1,9 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let menor = 0;
-for (const element of numbers) {
-  if (element - menor === element) {
-    menor = element;
-  }
-  if (element < menor) {
-    menor = element;
+
+for (const iterator of numbers) {
+  if (iterator - numbers[0] < 0) {
+    menor = iterator;
   }
 }
 console.log(menor);
