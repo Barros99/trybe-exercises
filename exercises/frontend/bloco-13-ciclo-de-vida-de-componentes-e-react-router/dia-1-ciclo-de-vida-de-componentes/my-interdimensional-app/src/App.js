6de6e2c61b0c51ad92a87103a32d817c8005ff7d
Barros99 <jeanmeneses@hotmail.com>
2021-07-29 19:08:52 -0300

    exercicio api componentDidMount

---
+++ exercises/frontend/bloco-13-ciclo-de-vida-de-componentes-e-react-router/dia-1-ciclo-de-vida-de-componentes/my-interdimensional-app/src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      characters: [],
+    };
+  }
+
+  fetchCharacters = () => {
+    fetch('https://rickandmortyapi.com/api/character')
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ characters: data.results });
+      });
+  };
+
+  componentDidMount() {
+    this.fetchCharacters();
+  }
+
+  render() {
+    const { characters } = this.state;
+    return (
+      <div className='App'>
+        <h1>Ricky and Morty Characters:</h1>
+        <div className='body'>
+          {characters.map(({ id, gender, species, status, name, image }) => {
+            return (
+              <div className='container' key={id}>
+                <h3>{name}</h3>
+                <img src={image} alt={name}></img>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
